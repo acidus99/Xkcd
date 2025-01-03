14f8291cd7099c58534742891e775ea7019a51ed
Acidus <acidus99@users.noreply.github.com>
2025-01-03 16:46:23 -0500

    merging old xkcd projects

---
+++ Xkcd.Api/XkcdApiResponse.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Xkcd.Api;
+
+public class XkcdApiResponse
+{
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+    
+    public string Alt { get; init; }
+
+    [JsonPropertyName("num")]
+    public int ComicId { get; init; }
+
+    [JsonPropertyName("img")]
+    public string ImageUrl { get; init; }
+
+    //use safe title to avoid HTML like in comic #472 
+    [JsonPropertyName("safe_title")]
+    public string Title { get; init; }
+
+    public string Month { get; init; }
+    public string Day { get; init; }
+    public string Year { get; init; }
+
+    [JsonIgnore]
+    public string Date => $"{Year}-{Pad(Month)}-{Pad(Day)}";
+
+    private string Pad(string s)
+        => s.Length == 1 ? "0" + s : s;
+        
+    public static XkcdApiResponse FromJson(string json)
+        => JsonSerializer.Deserialize<XkcdApiResponse>(json, JsonSerializerOptions);
+}
---
+++ Xkcd.Cgi/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Xkcd.Cgi;
+
+public class Program
+{
+    private const string ArchivePath = "public_root/xkcd/archive/";
+    private static readonly Regex AllDigits = new(@"^\d+$", RegexOptions.Compiled);
+
+    public static void Main(string[] args)
+    {
+        Console.Write("20 image/png\r\n");
+
+        var filename = GetImageForRequest();
+        Console.OpenStandardOutput().Write(File.ReadAllBytes(filename));
+    }
+
+    static string GetImageForRequest()
+    {
+        try
+        {
+            var q = Environment.GetEnvironmentVariable("QUERY_STRING") ?? "";
+            if (q == "latest")
+            {
+                return GetLatestImage();
+            }
+            if (AllDigits.IsMatch(q))
+            {
+                var comicId = Convert.ToInt32(q);
+                if (GetComicIds().Contains(comicId))
+                {
+                    return GetSpecificImage(comicId);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        //if nothing hit, pick a random image
+        return GetRandomImage();
+    }
+
+    static string GetRandomImage()
+    {
+        var comicIDs = GetComicIds();
+        
+        Random rand = new Random();
+        int index = rand.Next(comicIDs.Count);
+        return GetSpecificImage(comicIDs[index]);
+    }
+
+    static string GetSpecificImage(int comicId)
+        => $"{ArchivePath}{comicId}.png";
+
+    static string GetLatestImage()
+        => GetSpecificImage(GetComicIds().Max());
+
+    static List<int> GetComicIds()
+        => Directory.GetFiles(ArchivePath, "*.png")
+            .Select(x => Path.GetFileNameWithoutExtension(x))
+            .Where(x => AllDigits.IsMatch(x))
+            .Select(x => Convert.ToInt32(x)).ToList();
+}
